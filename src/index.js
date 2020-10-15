c047a308120cab3ca5e66bcb7298d0e9f6e553ad
Vitaliy <vetal.morenko@mail.ru>
2020-10-15 16:13:52 +0300

    refactor: new solution

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@
 module.exports = function towelSort (matrix) {
     if(typeof matrix === 'undefined' || matrix.length === 0) return []
 
-    let result = []
-    matrix.forEach((el, index) => {
-        (index + 1) % 2 === 0 ? el.sort((a, b) => a < b).forEach(el => result.push(el)) : el.forEach(e => result.push(e))
-    })
-    return result
+    return (
+        matrix.reduce((acc, curr, index) => acc.concat(index % 2 === 0 ? curr : curr.reverse()))
+    )
 }
