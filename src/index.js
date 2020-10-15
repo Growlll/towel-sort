e7cfbb748922aa8fb623d21d752a09c4c711759b
Vitaliy <vetal.morenko@mail.ru>
2020-10-15 15:02:35 +0300

    feat: add decision for task towel-sort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    if(typeof matrix === 'undefined' || matrix.length === 0) return []
+
+    const reducer = (acc, curr, index) => {
+        ((index + 1) % 2 === 0) ? curr.sort((a, b) => a < b).forEach(el => acc.push(el)) : curr.forEach(e => acc.push(e))
+        return acc
+    }
+    return (matrix.reduce(reducer, acc = []))
 }
